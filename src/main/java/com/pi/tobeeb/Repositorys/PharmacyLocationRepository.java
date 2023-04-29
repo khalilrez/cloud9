e37b255ef666209b163f357f3e26f0fc51b4bcd1
“rihab1478” <nabli.rihab@esprit.tn>
2023-04-29 06:34:42 +0200

    add search method

--- src/main/java/com/pi/tobeeb/Repositorys/PharmacyLocationRepository.java
+++ src/main/java/com/pi/tobeeb/Repositorys/PharmacyLocationRepository.java
@@ -1,8 +1,8 @@
 package com.pi.tobeeb.Repositorys;
 
 import com.pi.tobeeb.Entities.PharmacyLocation;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Repository
 
-public interface PharmacyLocationRepository extends CrudRepository<PharmacyLocation, Integer> {
+public interface PharmacyLocationRepository extends JpaRepository<PharmacyLocation, Integer> {
 
 
     @Query("SELECT pl FROM PharmacyLocation pl WHERE pl.name LIKE %:searchTerm%")
