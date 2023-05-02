6c4be20fa37d7ed484d5b85a1d0b65c580f379b8
bouthaynaa <bouthayna.daas@esprit.tn>
2023-05-02 19:16:28 +0200

    last push

--- src/main/java/com/pi/tobeeb/Entities/Message.java
+++ src/main/java/com/pi/tobeeb/Entities/Message.java
@@ -4,32 +4,95 @@ import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.time.LocalDateTime;
+import java.util.Date;
+
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@FieldDefaults(level = AccessLevel.PRIVATE)
-
+@Table(name = "messages")
 public class Message {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String text;
+
+    private Date date;
     @ManyToOne
-    private User sender;
+    @JoinColumn(name = "conversation_id")
+    private Conversation conversation;
 
     @ManyToOne
-    private User recipient;
+    @JoinColumn(name = "user_id")
+    private User user;
 
-    @Column(nullable = false)
-    private String content;
+    private String file;
 
-    @Column(nullable = false)
-    private LocalDateTime sentAt;
+    public Long getId() {
+        return id;
+    }
 
-    private MessageStatus status;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    @JsonIgnore
+    public Conversation getConversation() {
+        return conversation;
+    }
+
+    public void setConversation(Conversation conversation) {
+        this.conversation = conversation;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Message() {
+        super();
+    }
+
+    public Message(Long id, String text, Date date, Conversation conversation, User user, String file) {
+        super();
+        this.id = id;
+        this.text = text;
+        this.date = date;
+        this.conversation = conversation;
+        this.user = user;
+        this.file = file;
+    }
+
+
 }
--- src/main/java/com/pi/tobeeb/Repositorys/MessageRepository.java
+++ src/main/java/com/pi/tobeeb/Repositorys/MessageRepository.java
@@ -4,13 +4,14 @@ package com.pi.tobeeb.Repositorys;
 import com.pi.tobeeb.Entities.Message;
 import com.pi.tobeeb.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message,Long> {
-   List<Message> findBySenderAndRecipient(User sender, User recipient);
-    List<Message> findByRecipient(User recipient);
-
+    @Query (value= "SELECT * FROM `messages` where `conversation_id` = :idconver order by date ASC", nativeQuery =true)
+    List<Message> getMessageByConvId(@Param("idconver")Long idconver);
 }
